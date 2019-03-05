22a91025baa0df1affcc998d7531b840ad1eeb30
bhaskargandu <bhaskargandu515@gmail.com>
2019-03-05 15:41:30 +0800

    Add files via upload
    movie application

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+cd MovieCruiserAuthenticationService
+source ./env-variable.sh
+mvn clean package
+docker build -t user-app .
+cd ..
+cd MovieCruiserService
+source ./env-variable.sh
+mvn clean package
+docker build -t movie-app .
+cd ..
+
---
+++ project.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+cd MovieCruiserAuthenticationService
+source ./env-variable.sh
+cd ..
+
+cd MovieCruiserService
+source ./env-variable.sh
+cd ..
